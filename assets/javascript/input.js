41a441173e19c37c7aaef596108518ed3471d1df
Erin Giannette <emg4784@yahoo.com>
2016-09-20 21:06:56 -0500

    adding css and js

---
+++ assets/javascript/input.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDKjGS5SrSZ4ChDoi2F3wXCvaR6jYuW-k4",
+    authDomain: "petripper-be46b.firebaseapp.com",
+    databaseURL: "https://petripper-be46b.firebaseio.com",
+    storageBucket: "petripper-be46b.appspot.com",
+    messagingSenderId: "790872853827"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
