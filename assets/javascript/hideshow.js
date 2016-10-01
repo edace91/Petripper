2d44df5708a80aaafea1abe14e110ccc6509b9d8
EdReynolds3 <ed.reynolds@gmail.com>
2016-10-01 04:30:48 -0500

    recover crashed merge, add 2nd API

--- assets/javascript/hideshow.js
+++ assets/javascript/hideshow.js
@@ -2,49 +2,16 @@ $(document).ready(function() {
 
 	hideAll();
 	$('#homepage').show();
+	
 	function hideAll(){
 
-<<<<<<< HEAD
-		$("#homepage").css("display", "none");
-		$("#mappage").css("display", "none");
-		$("#input").css("display", "none");
-=======
 		$('#homepage').hide();
-		// $('#mappage').hide();
+		// $('#mapPage').hide();
 		$('#input').hide();
 
->>>>>>> mapping
-	}
-
+	};
 	$(".linkHomepage").on("click",function(){
 
-<<<<<<< HEAD
-		$("#homepage").css("display", "block");
-		$("#mappage").css("display", "none");
-		$("#input").css("display", "none");
-
-		//hideAll();
-		//$('#homepage').show();
-
-	})
-
-	$(".linkMappage").on("click",function(){
-
-		$("#mappage").css("display", "block");
-		$("#input").css("display", "none");
-		$("#homepage").css("display", "none");
-		//hideAll();
-		//$('#mappage').show();
-	})
-	$(".linkInputpage").on("click",function(){
-
-		$("#input").css("display", "block");
-		$("#mappage").css("display", "none");
-		$("#homepage").css("display", "none");
-		//hideAll();
-		//$('#input').show();
-	})
-=======
 		hideAll();
 		$('#homepage').show();
 	});
@@ -53,15 +20,16 @@ $(document).ready(function() {
 
 		hideAll();
 		$('#mappage').show();
+		// $('#mappanel').show();
+		// $('#map').show();
 	});
 	$(".linkInputpage").on("click",function(){
 
 		hideAll();
-		$('#input').show();
 		$('#mappage').hide();
+		$('#input').show();
 	});
 
 	// $("#petNameinfo > li").append("<li>" + nameinput + "<li>"+ ageinput);
->>>>>>> mapping
 
 });
---
+++ assets/javascript/hotels.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+	var queryURL = "http://terminal2.expedia.com:80/x/nlp/results?q=pet%20friendly%20and%20hotel%20in%20Austin&apikey=5dTnLH3PqgOJ4eGr7WLZBbtVJOkyWAWw";
+
+	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+	     console.log(response);
+	     console.log(response.result.hotels.length);
+	     var count=response.result.hotels.length;
+	     console.log(count);
+
+     for (var i=0; i<count;i++){
+	     console.log("Hotel name: ",response.result.hotels[i].name);
+	     //recommend hotel for pet friendliness
+	 }
+
+	}); 
+
+	// var count=response.result.hotels.length;
+
+
+	var queryURL2="http://terminal2.expedia.com:80/x/activities/search?location=Austin%2C%20Texas&startDate=2015-08-08&endDate=2015-08-18&apikey=5dTnLH3PqgOJ4eGr7WLZBbtVJOkyWAWw";
+	
+
+	$.ajax({url: queryURL2, method: 'GET'}).done(function(response) {
+	     console.log(response);
+
+	     //Recommend activities
+
+	     for (var i=0; i<11; i++){
+				console.log("activities: ",response.activities[i].title);
+				console.log("price: ",response.activities[i].fromPrice);
+				console.log("activity duration: ",response.activities[i].duration);
+	     }
+	});
+});
