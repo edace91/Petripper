22d8a29b4bace1f5852a72703eb34b85a5d64222
EdReynolds3 <ed.reynolds@gmail.com>
2016-09-30 21:27:14 -0500

    remove HEAD error comments

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,53 +1,19 @@
- // Google API key: AIzaSyCCRMTV-PPLYPwXh56TJDaiRVL4_nuUC-Y
-
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
       var map;
       var infowindow;
 
       function initMap() {
-        var yourLocation = {lat: 30.3800, lng: -97.7412};
+        var pyrmont = {lat: 30.3800, lng: -97.7412};
 
         map = new google.maps.Map(document.getElementById('map'), {
-          center: yourLocation,
+          center: pyrmont,
           zoom: 14
         });
 
-        //Center our point
-      //   var infoWindow = new google.maps.InfoWindow({map: map});
-      //   if (navigator.geolocation) {
-      //     navigator.geolocation.getCurrentPosition(function(position) {
-      //       var pos = {
-      //         lat: position.coords.latitude,
-      //         lng: position.coords.longitude
-      //       };
-
-      //       infoWindow.setPosition(pos);
-      //       infoWindow.setContent('Location found.');
-      //       map.setCenter(pos);
-      //     }, function() {
-      //       handleLocationError(true, infoWindow, map.getCenter());
-      //     });
-      //   } else {
-      //     // Browser doesn't support Geolocation
-      //     handleLocationError(false, infoWindow, map.getCenter());
-      //   }
-      // }
-
-      // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      //   infoWindow.setPosition(pos);
-      //   infoWindow.setContent(browserHasGeolocation ?
-      //                         'Error: The Geolocation service failed.' :
-      //                         'Error: Your browser doesn\'t support geolocation.');
-
-        //end of Center our point
 
         infowindow = new google.maps.InfoWindow();
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch({
-          location: yourLocation,
+          location: pyrmont,
           radius: 2000,
           type: ['veterinary_care']
           // type: ['pet_store']
@@ -100,16 +66,3 @@
           infowindow.open(map, this);
         });
       }
-    </script>
-  </head>
-  <body>
-    <div id="map"></div>
-
-
-
-
-
-
-
-
-
