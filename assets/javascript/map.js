f4b542b7e18af84da18d4ab40df24b7380683351
EdReynolds3 <ed.reynolds@gmail.com>
2016-09-30 21:58:22 -0500

    delete index.html (conflict)

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,3 +1,9 @@
+// $(document).ready(function() {
+
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
       var map;
       var infowindow;
 
@@ -66,3 +72,4 @@
           infowindow.open(map, this);
         });
       }
+// });
