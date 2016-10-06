3c72c9348ed6208325bf4c02f9ff940b7fe336cb
Erin Giannette <emg4784@yahoo.com>
2016-10-06 17:25:00 -0500

    adding validation

--- assets/javascript/input.js
+++ assets/javascript/input.js
@@ -20,44 +20,53 @@ $(".btn").on("click",function(){
 	//var gender = $("#genderId :selected").text();
 	var gender = $("input[name=Gender]:checked", "#genderbtn").val();
 	console.log("species " + species+" gender "+ gender);
-// create an object
-	var newProfile ={
-		name: petname,
-		age: petage,
-		chip: chip,
-		info: ownerinfo,
-		species: species,
-		gender: gender 
 
+	var errorMsg = checkFields(petname, petage, ownerinfo, species);
+
+	if(errorMsg != ''){
+		alert(errorMsg);
 	}
-	/*uploads new profile to the database*/
-	database.ref().push(newProfile);
+	else{ // if there are missing fields don't send info to database
 
-	// log everything to console
-	console.log(newProfile.name);
-	console.log(newProfile.age);
-	console.log(newProfile.chip);
-	console.log(newProfile.info);
-	console.log(newProfile.species);
-	console.log(newProfile.gender);
+		// create an object
+		var newProfile ={
+			name: petname,
+			age: petage,
+			chip: chip,
+			info: ownerinfo,
+			species: species,
+			gender: gender 
 
-	// clear each text box
-	$("#nameinput").val("");
-	$("#ageinput").val("");
-	$("#chipinput").val("");
-	$("#ownerinput").val("");
-	$(".animal").prop("checked", false);
-	$(".gender").prop("checked", false);
-	//$('select option:first-child').attr("selected", "selected");
+		}
+		/*uploads new profile to the database*/
+		database.ref().push(newProfile);
 
-	 $("petNameinfo").empty();
+		// log everything to console
+		console.log(newProfile.name);
+		console.log(newProfile.age);
+		console.log(newProfile.chip);
+		console.log(newProfile.info);
+		console.log(newProfile.species);
+		console.log(newProfile.gender);
+
+		// clear each text box
+		$("#nameinput").val("");
+		$("#ageinput").val("");
+		$("#chipinput").val("");
+		$("#ownerinput").val("");
+		$(".animal").prop("checked", false);
+		$(".gender").prop("checked", false);
+		//$('select option:first-child').attr("selected", "selected");
+
+	 	$("petNameinfo").empty();
   
-var list = $("#petNameinfo").append('<ul></ul>').find('ul');
-list.append('<li>'+ newProfile.name + '</li>');
-list.append('<li>'+ newProfile.age + '</li>');
+		var list = $("#petNameinfo").append('<ul></ul>').find('ul');
+		list.append('<li>'+ newProfile.name + '</li>');
+		list.append('<li>'+ newProfile.age + '</li>');
+	}
 
 	// Prevents moving to new page
-  return false;
+  	return false;
 });
 // Create Firebase event for adding into into to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey){
@@ -88,6 +97,39 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
 
 });
+
+function checkFields(petname, petage, ownerinfo, species){
+	var errorMsg = "Please fill in the blank fields:\n\n";
+	//var petname = $("#nameinput").val();
+	//var petage = $("#ageinput").val();
+
+	var nameBlank = false;
+	var ageBlank = false;
+	var ownerInfoBlank = false;
+	var speciesBlank = false;
+
+	if (petname == ''){
+		nameBlank = true;
+		errorMsg += "Pets Name\n";
+	}
+	if(petage == ''){
+		ageBlank = true;
+		errorMsg += "Pets Age\n";
+	}
+	if(ownerinfo == ''){
+		ownerInfoBlank = true;
+		errorMsg += "Owner info\n";
+	}
+	if(species == ''){
+		speciesBlank = true;
+		errorMsg += 'Species';
+	}
+	if(!nameBlank && !ageBlank && !ownerInfoBlank && !speciesBlank){
+		errorMsg = '';
+	}
+	return errorMsg;
+}
+
 
 
 
